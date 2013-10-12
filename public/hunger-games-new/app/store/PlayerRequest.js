aa6f48a8068b259c64a3ee0698f57f61812fc18d
DipakPanchal <11dipak.contact@gmail.com>
2013-10-12 16:57:01 +0530

    fixing isuse

--- public/hunger-games-new/app/store/PlayerRequest.js
+++
@@ -1,7 +0,0 @@
-Ext.define('HungerApp.store.PlayerRequest', {
-    extend: 'Ext.data.Store',
-    
-    config: {
-        model: 'HungerApp.model.Profile',
-	}
-});
--- public/hunger-games-new/app/store/ReviewList.js
+++
@@ -1,7 +0,0 @@
-Ext.define('HungerApp.store.ReviewList', {
-    extend: 'Ext.data.Store',
-    
-    config: {
-        model: 'HungerApp.model.ReviewList',
-    }
-});
